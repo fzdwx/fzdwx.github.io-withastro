b5d41d45a5b7c4b25d0f6acc761057787499c59b
fzdwx <likelovec@gmail.com>
2023-08-06 22:45:18 +0800

    Update hreader

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,70 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const SITE_CONFIG = {
+    header: {
+        title: "fzdwx",
+        icon: "https://avatars.githubusercontent.com/u/65269574?v=4",
+        description: "fzdwx's blog",
+    },
+    footer: {
+        copyRight: "2023 ~ forever | <a class='text-just-dark' href='https://github.com/fzdwx' target='_blank'>fzdwx</a> All Rights Reserved.",
+    },
+    github: {
+        owner: "fzdwx",
+        repo: "fzdwx.github.io",
+    },
+    feeds: [
+        {
+            url: "https://manateelazycat.github.io/feed.xml",
+            avatar: "https://manateelazycat.github.io/favicon.ico"
+        },
+        {
+            url: "http://feeds.feedburner.com/ruanyifeng",
+            avatar: "http://www.ruanyifeng.com/blog/images/person2_s.jpg"
+        },
+        {
+            url: "https://blog.codingnow.com/atom.xml",
+            avatar: "https://blog.codingnow.com/favicon.ico"
+        },
+        {
+            url: "https://www.skyzh.dev/posts/index.xml",
+            avatar: "https://www.skyzh.dev/skyzh.jpg"
+        },
+        {
+            url: "https://www.manjusaka.blog/atom.xml",
+            avatar: "https://user-images.githubusercontent.com/7054676/56820343-2fe1b580-687e-11e9-8f6f-778df3a8eafd.png"
+        },
+        {
+            url: "https://weekly.tw93.fun/rss.xml",
+            avatar: "https://avatars.githubusercontent.com/u/8736212?v=4"
+        },
+        {
+            url: "https://linux-china.davao.page/rss.xml",
+            avatar: "https://avatars.githubusercontent.com/u/46711?v=4"
+        }
+    ],
+    links: [
+        {
+            title: "Home",
+            url: "/",
+            icon: "ph:house-line-duotone",
+        },
+        {
+            title: "Blog",
+            url: "/blog",
+            icon: "mdi:text-box-multiple-outline",
+        },
+        {
+            title: "Feeds",
+            url: "/sub-feeds",
+            icon: "icon-park-outline:rss"
+        },
+        {
+            title: "Timeline",
+            url: "/timeline",
+            icon: undefined
+        }
+    ]
+}
---
+++ uno.config.ts
@@ -0,0 +1,18 @@
+// uno.config.ts
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+    // ...UnoCSS options
+    theme:{
+        colors:{
+            'aura-just': "#61ffca",
+            'just': "#10b981",
+            'just-light': "#34d399",
+            'just-lighter': "#6ee7b7",
+            'just-dark': "#059669",
+            'just-darker': "rgb(4, 120, 87)",
+            'codebg': "#f8f8f8",
+            'codetext': "#24292e"
+        }
+    }
+})
