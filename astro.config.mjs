4f603fa68ed5fe4d42c11ae50d629e449032f5c6
fzdwx <likelovec@gmail.com>
2023-08-07 01:43:44 +0800

    update use vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
-
 import image from "@astrojs/image";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fzdwx.vercel.app',
-  output:'hybrid',
+  output: 'hybrid',
   integrations: [mdx(), sitemap(), UnoCSS({
     injectReset: true
-  }), image()]
+  }), image()],
+  adapter: vercel()
 });
